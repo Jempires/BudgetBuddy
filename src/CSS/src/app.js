5b72d160ba98766033ffd588ea5f0ad9f49e920c
Jennifer <Rafter.jennifer@gmail.com>
2021-04-07 22:39:23 -0700

    Imported old code, restructing

---
+++ src/CSS/src/app.js
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<HTML lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Note-ly/CSS/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BudgetBuddy</title>
+</head>
+<body>
+
+
+
+
+
+
+</div>
+    <footer>
+        <h3>Created by Jempire</h3>
+    </footer> 
+
+
+
+
+
+</body>
+</HTML>
