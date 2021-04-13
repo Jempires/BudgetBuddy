a49a10fbb60509d47feaa4a044bd6bf45538e555
Jennifer <Rafter.jennifer@gmail.com>
2021-04-13 04:01:11 -0700

    figured out the javascript code for all operaions.

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ const budnamamt = document.getElementById("budnamamt");
 
 const mybudamt = document.getElementById("mybudamt");
 const balance = document.getElementById("balance");
-
+const expAmt = document.getElementById("expAmt");
  
 function inputbudget(budamt) {
+    expAmt.innerHTML = "";
     mybudamt.innerText = budamt;
     balance.innerText = budamt;
     budamt.value = "";
@@ -21,4 +22,76 @@ inputbudget(budamt.value);
 const expinput = document.getElementById("expinput");
 let type = document.getElementById("type");
 let cost = document.getElementById("cost");
-let
+let id=0;
+let description = [];
+
+function ExpAdd(name, number) {
+    const inputExp = {
+        id: id,
+        name: name,
+        number: parseInt(number),
+    };
+    description.push(inputExp);
+    displayExp(description);
+    id++;
+    type.value = "";
+    cost.value = "";
+}
+
+expinput.addEventListener("submit", (e) => {
+    e.preventDefault();
+    ExpAdd(type.value, cost.value);
+});
+
+function displayExp(description) {
+    expAmt.innerHTML = null;
+    for (i = 0; i < description.length; i++) {
+        expAmt.innerHTML += `
+        <div class="expAmt" id="${description[i].id}">
+        <div id="Expense"><p>${description[i].name}</p></div>
+        <div id="Amount"><p> <span>$ </span> ${description[i].number}</p></div>
+   `;
+ }
+ calcExp();
+ displayExpenses.style.display = "block";
+}
+
+function calcExp() {
+    let total = 0;
+    for (i = 0; i < description.length; i++) {
+        total = description[i].number + total;
+    }
+    balupdate(total);
+}
+
+function balupdate(total) {
+    balance.innerText =
+    parseInt(mybudamt.innerText) - parseInt(total);
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
