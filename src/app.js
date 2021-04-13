1e612f479c4de2763859f479722a14135a393145
Jennifer <Rafter.jennifer@gmail.com>
2021-04-13 02:41:03 -0700

    fixed style, positions, layout, added buttons and text fields

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
 
+const budamt = document.getElementById("budamt");
+const budnamamt = document.getElementById("budnamamt");
 
+const mybudamt = document.getElementById("mybudamt");
+const balance = document.getElementById("balance");
+
+ 
+function inputbudget(budamt) {
+    mybudamt.innerText = budamt;
+    balance.innerText = budamt;
+    budamt.value = "";
+};
+
+budnamamt.addEventListener("submit", (e) => {
+e.preventDefault();
+inputbudget(budamt.value);
+});
+
+
+const expinput = document.getElementById("expinput");
+let type = document.getElementById("type");
+let cost = document.getElementById("cost");
+let
